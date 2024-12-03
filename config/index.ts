666219bc37c93f135705694ed20bb45a058da03c
mayh <xiyizia@163.com>
2024-12-03 16:29:35 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -8,7 +8,7 @@ export const APP_INFO: AppInfo = {
   description: 'App description',
   copyright: '',
   privacy_policy: '',
-  default_language: 'en-US',
+  default_language: 'zh-Hans',
 }
 
 export const API_PREFIX = '/api'
